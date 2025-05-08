6bdb6e1e8b6f8545512bb67a5859bbfda447556e
Jacob Harris <mail@jacobharr.is>
2025-05-08 11:02:24 -0400

    New model where I move events back to agencies

--- data/scripts/validate_events_yaml.rb
+++ data/scripts/validate_events_yaml.rb
@@ -5,24 +5,65 @@ require 'date'
 require 'edtf'
 require 'shortuuid'
 
-events_file = File.join(File.dirname(__FILE__), '..', 'raw_data', 'events.yaml')
-out_file = File.join(File.dirname(__FILE__), '..', 'raw_data', 'processed_events.yaml')
+def validate_agency(a)
+end
 
-events = YAML.unsafe_load(File.read(events_file), symbolize_names: true)
-
-events.each do |e|
+def validate_event(e)
   unless e[:id]
     id = SecureRandom.uuid
     e[:id] = ShortUUID.shorten(id)[0...8]
   end
+
+  return e
 end
 
-out = events.each_with_index.sort_by { |e, idx| [Date.edtf(e[:date].to_s), idx] }.map(&:first)
+interagency_file = File.join(File.dirname(__FILE__), '..', 'raw_data', 'interagency.yaml')
+agencies_file = File.join(File.dirname(__FILE__), '..', 'raw_data', 'agencies.yaml')
 
-File.open(out_file, 'w') do |file|
-  schema_hdr = "# yaml-language-server: $schema=../schemas/events-file.json\n"
-  out_yaml = YAML.dump(out, line_width: 150, stringify_names: true, header: false)
+interagency = YAML.unsafe_load(File.read(interagency_file), symbolize_names: true)
+agencies = YAML.unsafe_load(File.read(agencies_file), symbolize_names: true)
+
+events = []
+interagency.each do |e|
+  events.append(validate_event(e))
+end
+
+agencies.each do |a|
+  validate_agency(a)
+
+  a[:events] ||= []
+  a[:events].each do |e|
+    e[:agency] ||= a.id
+    events.append(validate_event(e))
+  end
+
+  # Don't worry, we'll add them back
+  a[:events] = []
+end
+
+sorted_events = events.each_with_index.sort_by { |e, idx| [Date.edtf(e[:date].to_s), idx] }.map(&:first)
+
+interagency_events = []
+
+sorted_events.each do |event|
+  event_agencies = Array(event[:agency])
+  if event_agencies.size > 1
+    interagency_events.append(event)
+  else
+    agency_id = event_agencies[0]
+    agency = agencies.find {|a| a[:id] == agency_id }
+    agency[:events].append(event)
+  end
+end
+
+File.open(interagency_file, 'w') do |file|
+  schema_hdr = "# yaml-language-server: $schema=../schemas/interagency-events-file.json\n"
+  out_yaml = YAML.dump(interagency_events, line_width: 150, stringify_names: true, header: false)
   file.write(schema_hdr, out_yaml.gsub(/^- /, "\n- "))
 end
 
-File.rename(out_file, events_file)
+File.open(agencies_file, 'w') do |file|
+  schema_hdr = "# yaml-language-server: $schema=../schemas/agencies-file.json\n"
+  out_yaml = YAML.dump(agencies, line_width: 150, stringify_names: true, header: false)
+  file.write(schema_hdr, out_yaml.gsub(/^- /, "\n- "))
+end
