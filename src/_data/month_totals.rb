ad7c7467df72b02a097978bcb1c8b33181352fd4
Jacob Harris <mail@jacobharr.is>
2026-02-02 16:35:24 -0500

    Some data tweaks

--- src/_data/month_totals.rb
+++ src/_data/month_totals.rb
@@ -33,6 +33,8 @@ Person.each do |person|
                   (start_date..iter_end_date).map { |x| x.strftime(key_format) }.uniq.sort
                 end
 
+  puts "#{person.name} #{person_keys}"
+
   person_keys.each do |key|
     totals[key][:count] += 1
   end
