4aa15a5a4996457b53cdcd199344b201dec172ab
Jacob Harris <mail@jacobharr.is>
2026-04-09 06:55:51 -0400

    Trying collapsible info elements

--- src/_components/month_grid/system_grants.rb
+++ src/_components/month_grid/system_grants.rb
@@ -9,7 +9,19 @@ module MonthGrid
     end
 
     def template
-      text -> { '' }
+      return text -> { '' } if @roles.empty?
+
+      @by_system_access = roles.group_by { |r| [r.govt_system, r.type] }
+
+      html_map(@by_system_access) do |grp|
+        grp[0]
+        grp[1]
+
+        <<~HTML
+          <div><i class="fa-sharp fa-solid fa-display"></i></div>
+          <div></div>
+        HTML
+      end
     end
   end
 end
